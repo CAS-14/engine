c408971a9dd090d783f421ee1849cd33ec3fb330
weirdcease <78571139+weirdcease@users.noreply.github.com>
2023-04-27 14:29:03 -0400

    dsfsd

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import time
 # this game is (c) 2023 by weirdcease
 
 DIRECTIONS = ["n", "s", "e", "w", "ne", "nw", "se", "sw"]
+LIFESQUARE = True
 
 def asset(filename):
     return os.path.join("assets", filename)
@@ -27,6 +28,9 @@ class App:
         self.speed = 60
         self.move_step = 10
 
+        self.texts = []
+        self.blits = []
+
     def check_events(self):
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -36,6 +40,15 @@ class App:
                     if event.key:
                         return event.key
 
+    def get_text(self, content: str, color: tuple = None, background: tuple = None, font: pygame.font.Font = None):
+        for text in self.texts:
+            if text.content == content and ((not color) or text.color == color) and ((not background) or text.background == background) and ((not font) or text.font == font):
+                return text
+            
+        new_text = Text(self, content, color=color, background=background, font=font)
+        self.texts.append(new_text)
+        return new_text
+
     def main(self):
         pygame.init()
 
@@ -43,50 +56,89 @@ class App:
         pygame.display.set_icon(self.logo)
         pygame.display.set_caption("minimal program")
      
-        self.font = pygame.font.Font(asset("alagard.ttf"), 14)
-
-        self.text_welcome = Text(self, content="Welcome", pos=(50, 50))
-        self.text_newgame = Text(self, content="New Game", pos=(50, 50))
-        self.text_options = Text(self, content="Options", pos=(50, 50))
-
         self.screen = pygame.display.set_mode(self.size)
-        
-        self.sprites = []
-        
-        for i in range(200):
-            x = random.randint(100, self.width-100)
-            y = random.randint(100, self.height-100)
-            vel_x = random.randint(-10, 10)
-            vel_y = random.randint(-10, 10)
 
-            projectile = Projectile(self, image="catsmirk.png", pos=(x, y), velocity=(vel_x, vel_y), bounce=True)
-
-            self.sprites.append(projectile)
+        self.font = pygame.font.Font(asset("alagard.ttf"), 50)
         
-        self.loop = self.scn_menu
+        self.loop = self.main_menu
+        
+        # set scenes as non-initialized
+        self.init_cats = False
+
+        # init stuff
+        self.menu_index = 0
+        self.initialized = []
+        self.times = []
 
         self.running = True
 
         # GAME LOOP
         while self.running:
             key = self.check_events()
+            if key: print(f"{key} pressed!")
 
             self.loop(key)
 
+            if LIFESQUARE:
+                self.screen.fill(tuple([random.randint(0, 255) for i in range(3)]), rect=(self.width-30, self.height-30, 20, 20))
+
             pygame.display.flip()
 
+            if len(self.times) > 10:
+                self.times.pop(1)
+            self.times.append(time.time())
             time.sleep(1 / self.speed)
 
-    def scn_menu(self, key):
+    def opt_quit(self, key):
+        self.running = False
+
+    def main_menu(self, key):
+        menu_items = ["New Game", "Options", "Music Room", "Cat Room", "Quit"]
+        menu_funcs = [None, None, None, self.scn_cats, self.opt_quit]
+
+        if key == pygame.K_DOWN: 
+            self.menu_index += 1
+        elif key == pygame.K_UP: 
+            self.menu_index -= 1
+        elif key == pygame.K_RETURN:
+            self.loop = menu_funcs[self.menu_index]
+            self.menu_index = 0
+        
+        if self.menu_index > len(menu_items) - 1: 
+            self.menu_index = 0
+        elif self.menu_index < 0: 
+            self.menu_index = len(menu_items) - 1
+
         self.screen.fill((64, 0, 0))
-        self.screen.fill((128, 0, 0), rect=(200, 70, 50, self.height-120))
+        self.screen.fill((128, 0, 0), rect=(50, self.height-120, 200, 70))
 
-        self.text_welcome.blit()
+        for i in range(len(menu_items)):
+            if i == self.menu_index:
+                color = (30, 10, 150)
+                background = (160, 0, 0)
+            else:
+                color = (250, 40, 5)
+                background = None
 
-    def scn_cats(self):
+            text = self.get_text(menu_items[i], color=color, background=background)
+            text.blit((70, 70 + 30 * i))
+
+    def scn_cats(self, key):
+        if not self.init_cats:
+            for i in range(200):
+                x = random.randint(100, self.width-100)
+                y = random.randint(100, self.height-100)
+                vel_x = random.randint(-10, 10)
+                vel_y = random.randint(-10, 10)
+
+                projectile = Projectile(self, image="catsmirk.png", pos=(x, y), velocity=(vel_x, vel_y), bounce=True)
+                self.blits.append(projectile)
+
+            self.init_cats = True
+
         self.screen.fill((64, 0, 0))
 
-        for sprite in self.sprites:
+        for sprite in self.blits:
             if isinstance(sprite, Projectile):
                 sprite.step()
 
@@ -98,28 +150,34 @@ class Positional:
         self.app = app
         self.x, self.y = pos
 
-    def blit(self):
-        self.app.screen.blit(self.object, (self.x, self.y))
+    def blit(self, pos: tuple = None):
+        if not pos:
+            pos = self.x, self.y
+        self.app.screen.blit(self.object, pos)
 
 
 class Text(Positional):
-    def __init__(self, *args, content: str, color: tuple = None, background: tuple = None, font: pygame.font.Font = None, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, app: App, content: str, *, pos: tuple = (1, 1), color: tuple = None, background: tuple = None, font: pygame.font.Font = None, antialias: bool = True):
+        super().__init__(app, pos=pos)
 
         if not color:
             color = (30, 10, 100)
+
+        self.content = content
+        self.color = color
+        self.background = background
 
         if font:
             self.font = font
         else:
             self.font = self.app.font
 
-        self.object = self.font.render(content, True, color, background)
+        self.object = self.font.render(self.content, antialias, self.color, self.background)
 
 
 class Sprite(Positional):
-    def  __init__(self, *args, image: str, **kwargs):
-        super().__init__(*args, **kwargs)
+    def  __init__(self, app: App, *, image: str, pos: tuple = (1, 1)):
+        super().__init__(app, pos=pos)
 
         self.object = self.image = load_image(image)
 
@@ -143,8 +201,8 @@ class Sprite(Positional):
 
 
 class Projectile(Sprite):
-    def __init__(self, *args, velocity: tuple, bounce: bool = False, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, app: App, *, image: str, pos: tuple = (1, 1), velocity: tuple = (0, 0), bounce: bool = False):
+        super().__init__(app, image=image, pos=pos)
 
         self.vel_x, self.vel_y = velocity
         self.vel_x_start, self.vel_y_start = self.vel_x, self.vel_y
